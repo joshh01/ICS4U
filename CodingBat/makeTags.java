29f3446e41cd857513657dfb6897a612eb55d6c6
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 09:14:00 -0500

    Create makeTags.java

---
+++ CodingBat/makeTags.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: makeTags
+* Program makes html tags.
+*/
+public String makeTags(String tag, String word) {
+  return "<" + tag + ">" + word + "</" + tag + ">";
+}//end makeTags
