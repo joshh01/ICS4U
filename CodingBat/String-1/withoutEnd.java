3bf5c8119d970783786adbac916a9f9c0d4320b1
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 09:41:51 -0500

    Update withoutEnd.java

--- CodingBat/String-1/withoutEnd.java
+++ CodingBat/String-1/withoutEnd.java
@@ -1,8 +1,8 @@
 /*
 * Joshua de Souza
 * February 7, 2022
-* CodingBat String1: firstHalf
-* Program returns a string with the first half of a word.
+* CodingBat String1: withoutEnd
+* Program returns a string without the first and last letter.
 */
 public String withoutEnd(String str) {
   return str.substring(1, str.length() - 1);
