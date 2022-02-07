d97dbbd22bac57bfd62feba71baa36d9948b213a
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 10:48:39 -0500

    Create middleThree.java

---
+++ CodingBat/String-1/middleThree.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: middleThree
+* Program returns the middle three characers of a string.
+*/
+public String middleThree(String str) {
+  return str.substring(str.length() / 2 - 1, str.length() / 2 + 2);
+}//end middleThree
