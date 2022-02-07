bb43dd8aca82b11fae54ddd696b6e5d0770872d7
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 09:41:07 -0500

    Create withoutEnd.java

---
+++ CodingBat/String-1/withoutEnd.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: firstHalf
+* Program returns a string with the first half of a word.
+*/
+public String withoutEnd(String str) {
+  return str.substring(1, str.length() - 1);
+}//end withoutEnd
