20a1e69246ba13970d390adc4b656e40e7592f9b
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 09:38:09 -0500

    Create firstHalf.java

---
+++ CodingBat/String-1/firstHalf.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: firstHalf
+* Program returns a string with the first half of a word.
+*/
+public String firstHalf(String str) {
+  return str.substring(0, str.length() / 2);
+}//end firstHalf
