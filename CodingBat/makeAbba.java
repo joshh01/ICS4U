b1349cb82f6dda10445e2e75684e43781262e51a
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 09:10:12 -0500

    Create makeAbba.java

---
+++ CodingBat/makeAbba.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: makeAbba
+* program puts two strings together.
+*/
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
