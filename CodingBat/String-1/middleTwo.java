fa095a8d1773dc69a76812470e72c53e77f3a834
Josh <88389161+joshh01@users.noreply.github.com>
2022-02-07 10:26:54 -0500

    Create middleTwo.java

---
+++ CodingBat/String-1/middleTwo.java
@@ -0,0 +1,9 @@
+/*
+* Joshua de Souza
+* February 7, 2022
+* CodingBat String1: middleTwo
+* Program returns strings with only the middle two characters.
+*/
+public String middleTwo(String str) {
+  return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
+}//end middleTwo
