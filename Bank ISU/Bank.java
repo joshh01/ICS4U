7f02a6a2e1e61a87f7717874aca4a337fdd407d6
Josh <88389161+joshh01@users.noreply.github.com>
2022-04-04 09:58:19 -0400

    Update Bank.java

--- Bank ISU/Bank.java
+++ Bank ISU/Bank.java
@@ -4,6 +4,7 @@ public class Bank
 	private String name;
 	private String address;
 	private String phoneNumber;
+	private String masterPin;
 	ArrayList<Accounts> accounts;
 	//Default Constructor
 	public Bank()
@@ -11,13 +12,16 @@ public class Bank
 		name = "TD Canada Trust";
 		address = "3477 Sheppard Ave E, Scarborough, ON M1T 3K6";
 		phoneNumber = "+1 (416)-291-9566";
+		//masterPin = (int)Math.floor(Math.random() * 9000 + 1000) + "";
+		masterPin = "0000";
 		accounts = new ArrayList<Accounts>();
 	}//end Bank() default
-	public Bank(String name, String address, String phoneNumber, ArrayList<Accounts> accounts)
+	public Bank(String name, String address, String phoneNumber, String masterPin,  ArrayList<Accounts> accounts)
 	{
 		this.name = name;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
+		this.masterPin = masterPin;
 		this.accounts = accounts;
 	}//end Bank()
 	public void addAccount(Accounts newAcc)
@@ -40,6 +44,10 @@ public class Bank
 	{
 		return phoneNumber;
 	}//end getPhoneNumber()
+	public String getMasterPin()
+	{
+		return masterPin;
+	}//end getMasterPin()
 	public String accountList()
 	{
 		String str = "";
