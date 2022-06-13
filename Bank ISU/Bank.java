db81febfcfde468e461223e1fdd1506f5aeb062e
Josh <88389161+joshh01@users.noreply.github.com>
2022-06-13 09:42:17 -0400

    Update Bank.java

--- Bank ISU/Bank.java
+++ Bank ISU/Bank.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Bank 
 {
 	private String name;
@@ -78,12 +81,78 @@ public class Bank
 		}
 		return AL;
 	}
+	public boolean searchAccount(String accNum)
+	{
+		for(int i = 0; i < accountAL().size(); i++)
+    	{
+    		if(accountAL().get(i).getAccountNumber().contentEquals(accNum)) 
+    		{
+    			return true;
+    		}//end if loop
+    	}//end for loop
+		return false;
+	}//end searchAccount()
+	public Accounts getAccount(String accNum)
+	{
+		int index = -1;
+		for(int i = 0; i < accountAL().size(); i++)
+    	{
+    		if(accountAL().get(i).getAccountNumber().equals(accNum)) 
+    		{
+    			//System.out.println(bank.accountAL().get(i).getAccountNumber());
+    			index = i;
+    		}//end if loop
+    	}//end for loop
+		return accountAL().get(index);
+	}//end getAccount()
+	public void transferFunds(Accounts accSending)
+	{
+		double amount;
+		String accountNumber = "";
+		SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss");
+        String time = date.format(new Date());
+        Scanner scanner = new Scanner(System.in);
+		System.out.print("What account [ID] would you like to send money to? ");
+		accountNumber = scanner.nextLine();
+		if(searchAccount(accountNumber))
+		{
+			Accounts accReceiving = getAccount(accountNumber);
+			System.out.print("How much would you like to send to " + accReceiving.getName() + "? ");
+			amount = Double.parseDouble(scanner.nextLine());
+			if(amount > accSending.getBalance()) 
+			{
+				System.out.println("You do not have this much money in your account. Retry with a lower amount.\n");
+			}//end inner if
+			else
+			{
+				System.out.println();
+				accSending.setBalance(accSending.getBalance() - amount);
+				accReceiving.setBalance(accReceiving.getBalance() + amount);
+				System.out.println("$" + amount + " has successfully been sent to: " + accReceiving.getName() + ".\n");
+				accSending.addTransaction(time + " | " + accSending.getAccountNumber() + " | NEW TRANSFER: (-) $" + amount + ". BALANCE [" + (accSending.getBalance() + amount) + "] --> [" + (accSending.getBalance()) + "]");
+				accReceiving.addTransaction(time + " | " + accountNumber + " | NEW TRANSFER: (+) $" + amount + ". BALANCE [" + (accReceiving.getBalance() - amount) + "] --> [" + (accReceiving.getBalance()) + "]");
+			}//end else
+		}//end outer if
+		else
+		{
+			System.out.println("The account number you have entered [" + accountNumber + "] is invalid. Please restart and try again.\n");
+		}//end else
+		scanner.close();
+	}//end transferFunds()
+	public boolean validatePassword(String accNum, String password)
+	{
+		if(searchAccount(accNum) && (getAccount(accNum).getPassword().equals(password)))
+		{
+			return true;
+		}//end if
+		return false;
+	}//end validatePassword()
 	public String bankInfo()
 	{
 		return "Name: " + name + "\n" + 
 				"Address: " + address + "\n" + 
 				"Phone Number: " + phoneNumber + "\n" + 
-				"Number of Accounts: " + accountList().length() + "\n";
+				"Number of Accounts: " + accountAL().size() + "\n";
 	}//end bankInfo()
 	public String toString()
 	{
